0ac7f57b6c1fef62fe343cb8ae038561d20eb8bc
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-03-21 14:52:14 +0100

    fix schema import - fixes cfx realmstore (#20)
    * fix schema import - fixes cfx realmstore
    
    * fix yapf

--- cfxdb/__init__.py
+++ cfxdb/__init__.py
@@ -8,9 +8,11 @@
 from ._version import __version__
 from .common import address, uint256, unpack_uint256, pack_uint256,\
     uint128, unpack_uint128, pack_uint128
+from . import schema
 
 __all__ = (
     '__version__',
+    'schema',
     'address',
     'uint256',
     'pack_uint256',
